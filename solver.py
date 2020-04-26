a24940715f463e0f12f1ea3995e2bf157ac45456
Weishi Li <lewislvs@hotmail.com>
2020-04-25 22:17:16 -0700

    genST done

--- solver.py
+++ solver.py
@@ -16,6 +16,127 @@ def solve(G):
     # TODO: your code here!
     pass
 
+def genST(G):
+    output = []
+    List = {G}
+    for u, v in G.edges:
+        G.edges[u, v]['property'] = 'normal'
+    List = {G}
+    G.graph['MST'] = KruskalMST(G)
+    MST = {tuple(G.graph['MST'])}
+    while len(MST) != 0 :
+        temp = min(List, key = lambda g: g.graph['cost'])
+        output.append(temp.graph['MST'])
+        List.remove(temp)
+        MST.remove(tuple(temp.graph['MST']))
+        Partition(temp, List, MST)
+    return output
+
+
+
+def Partition(P, List, MST):
+    P1 = nx.Graph()
+    P2 = nx.Graph()
+    P1 = P.copy()
+    P2 = P.copy()
+    for u, v in P.graph['MST']:
+        if P.edges[u, v]['property'] == 'normal':
+            P1.edges[u, v]['property'] = 'excluded'
+            P2.edges[u, v]['property'] = 'included'
+            MSTP1 = KruskalMST(P1)
+            P1.graph['MST'] = MSTP1
+            #check if P1 is connected
+            P3 = P1.copy()
+            for u, v in P1.edges:
+                if P1.edges[u, v]['property'] == 'excluded':
+                    P3.remove_edge(u, v)
+            if len(list(nx.dfs_edges(P3, source=1))) == P3.number_of_nodes() - 1:
+                List.add(P1)
+                MST.add(tuple(MSTP1))
+            P1 = P2.copy()
+
+def union(G, x, y):
+    xroot = find(G, x)
+    yroot = find(G, y)
+
+    # Attach smaller rank tree under root of
+    # high rank tree (Union by Rank)
+    if G.nodes[xroot]['rank'] < G.nodes[yroot]['rank']:
+        G.nodes[xroot]['parent'] = yroot
+    elif G.nodes[xroot]['rank'] > G.nodes[yroot]['rank']:
+        G.nodes[yroot]['parent'] = xroot
+
+    # If ranks are same, then make one as root
+    # and increment its rank by one
+    else :
+        G.nodes[yroot]['parent'] = xroot
+        G.nodes[xroot]['rank'] += 1
+
+def find(G, i):
+    if G.nodes[i]['parent'] == i:
+        return i
+    return find(G, G.nodes[i]['parent'])
+
+def KruskalMST(P):
+    G = P.copy()
+    cost = 0
+
+    normal_edges = [] #store all the normal edges
+    result =[] #This will store the resultant MST
+    i = 0 # An index variable, used for sorted edges
+    e = 0 # An index variable, used for result[]
+
+    for u, v in G.edges:
+        if G.edges[u, v]['property'] == 'included':
+            result.append((u, v))
+            e += 1
+        elif G.edges[u, v]['property'] == 'excluded':
+            G.edges[u, v]['weight'] = 1000
+            normal_edges.append((u, v))
+        else: normal_edges.append((u, v))
+
+        # Step 1:  Sort all the edges in non-decreasing
+            # order of their
+            # weight.  If we are not allowed to change the
+            # given graph, we can create a copy of graph
+    if len(normal_edges) == 0:
+        return result
+
+    sortedges = sorted( normal_edges, key=lambda edge: G.edges[edge]['weight'])
+    # Create V subsets with single elements
+    for node in G.nodes:
+        G.nodes[node]['parent'] = node
+        G.nodes[node]['rank'] = 0
+
+
+    # Number of edges to be taken is equal to V-1
+    while e < G.number_of_nodes() - 1:
+
+        # Step 2: Pick the smallest edge and increment
+                # the index for next iteration
+        if i > len(sortedges) - 1:
+            return []
+        u,v =  sortedges[i]
+
+        i = i + 1
+        x = find(G, u)
+        y = find(G ,v)
+
+        # If including this edge does't cause cycle,
+                    # include it in result and increment the index
+                    # of result for next edge
+        if x != y:
+            e = e + 1
+            result.append((u,v))
+            union(G, x, y)
+        # Else discard the edge
+    for i,j in result:
+        cost += P.edges[i, j]['weight']
+    P.graph['cost'] = cost
+    P.graph['MST'] = result
+    return result
+
+
 
 # Here's an example of how to run your solver.
 
