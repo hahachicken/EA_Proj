5020170efef80b0961ef3967a99957966857f1a3
hahachicken <weixiao.zhan00@gmail.com>
2020-05-03 13:35:11 -0700

    mul-computer

--- parse.py
+++ parse.py
@@ -94,7 +94,7 @@ def read_output_file(path, G):
             w["weight"] = edge_in_G["weight"]
         T.add_nodes_from(nodes)
 
-        assert utility.is_valid_network(G, T)
+        assert Utility.is_valid_network(G, T)
 
         return T
 
